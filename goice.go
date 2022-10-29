536db1608b38e6a27891f0c241fc164e0d989d99
Andrius Tamasauskas <andrius.tamasauskas@crowdhouse.ch>
2022-10-29 22:23:22 +0200

    add random action route

--- goice.go
+++ goice.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,6 +10,15 @@ import (
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/tools/rest"
 )
+
+var allowValuesMap map[string]struct{} = map[string]struct{}{
+	"wyr":      {},
+	"truth":    {},
+	"dare":     {},
+	"never":    {},
+	"paranoia": {},
+	"draw":     {},
+}
 
 func main() {
 	app := pocketbase.New()
@@ -25,6 +35,32 @@ func main() {
 				}{}
 				queryErr := app.Dao().DB().
 					NewQuery("SELECT quote, author FROM quotes ORDER BY RANDOM() LIMIT 1").
+					One(&result)
+				if queryErr != nil {
+					return rest.NewBadRequestError("Failed to fetch.", queryErr)
+				}
+				return c.JSON(200, result)
+			},
+			Name: "",
+		})
+
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/action",
+			Handler: func(c echo.Context) error {
+				table := c.FormValue("table")
+				result := struct {
+					Action string `db:"action" json:"action"`
+				}{}
+
+				_, ok := allowValuesMap[table]
+				if !ok {
+					return rest.NewBadRequestError("Invalid table.", nil)
+				}
+
+				fmt.Printf("Table: %v", table)
+				queryErr := app.Dao().DB().
+					NewQuery(fmt.Sprintf("SELECT action FROM %s WHERE enabled = True ORDER BY RANDOM() LIMIT 1", table)).
 					One(&result)
 				if queryErr != nil {
 					return rest.NewBadRequestError("Failed to fetch.", queryErr)
